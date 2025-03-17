b0592b37d9315141315de33fc5e6a5f85fe67257
devtobi <28538704+devtobi@users.noreply.github.com>
2025-03-17 19:20:30 +0100

    :lipstick: Added Roboto font configuration

--- browser-extension/src/entrypoints/background.ts
+++ browser-extension/src/entrypoints/background.ts
@@ -2,8 +2,8 @@ import { browser } from "wxt/browser";
 import { defineBackground } from "wxt/sandbox";
 
 export default defineBackground({
-	type: 'module',
-	main() {
-	  console.debug("Hello background!", { id: browser.runtime.id });
-	},
+  type: "module",
+  main() {
+    console.debug("Hello background!", { id: browser.runtime.id });
+  },
 });
