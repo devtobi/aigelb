901438f94ab4948b96efddad2211b972234d3b1d
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-17 12:02:16 +0200

    :recycle: Refactord logging into dedicated Logging Service

--- evaluation/src/helper/__init__.py
+++ evaluation/src/helper/__init__.py
@@ -3,14 +3,13 @@ from typing import List
 from .confirm import confirm_action
 from .csv import from_csv
 from .file import get_absolute_path, get_filename, get_files
-from .logger import get_logger, log_list
+from .logging_service import LoggingService
 from .metric import Metric
 from .model import Model, get_model_cache_dir
 
 __all__: List[str] = [
   "confirm_action",
-  "get_logger",
-  "log_list",
+  "LoggingService",
   "from_csv",
   "get_absolute_path",
   "get_files",
--- evaluation/src/helper/confirm.py
+++ evaluation/src/helper/confirm.py
@@ -1,12 +1,10 @@
-from logging import Logger
+from .logging_service import LoggingService
 
 
-def confirm_action(logger: Logger, question: str) -> bool:
+def confirm_action(question: str) -> bool:
     entered: str = "none"
     while entered != "exit" and entered != "":
-        logger.info(
-            f"{question} Press ENTER to confirm... (Type 'exit' or hit Ctrl+C to exit.)"
-        )
+        LoggingService.info(f"{question} Press ENTER to confirm... (Type 'exit' or hit Ctrl+C to exit.)")
         try:
             entered = input()
         except KeyboardInterrupt:
