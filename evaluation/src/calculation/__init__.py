e8b38e83f616b644424de1c6bdedc3c946f684f1
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-19 23:19:20 +0200

    :label: Added datatype to store a calculation result for one model

--- evaluation/src/calculation/__init__.py
+++ evaluation/src/calculation/__init__.py
@@ -1,7 +1,9 @@
 from typing import List
 
 from .calculation_service import CalculationService
+from .calculation_result import CalculationResult
 
 __all__: List[str] = [
-  "CalculationService"
+  "CalculationService",
+  "CalculationResult"
 ]
---
+++ evaluation/src/calculation/calculation_result.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Iterator, Tuple, Union
+
+
+@dataclass
+class CalculationResult:
+
+    _model_name: str
+    _metric_results: dict[str, float]
+
+    @property
+    def model_name(self) -> str:
+      return self._model_name
+
+    @property
+    def metric_results(self) -> dict[str, float]:
+      return self._metric_results
+
+    def to_dict(self) -> dict:
+      return {'_model_name': self._model_name, **self._metric_results}
+
+    def __iter__(self) -> Iterator[Union[str, bool]]:
+        return iter(self._as_tuple())
+
+    def __str__(self) -> str:
+        return (
+            f"Result for {self._model_name} with {self._metric_results}"
+        )
+
+    def _as_tuple(self) -> Tuple[str, dict[str, float]]:
+      return self._model_name, self._metric_results
+
+
+
+
