9fea4e1fc68c82922e55e2266ad0bba10b7fb549
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-19 23:57:03 +0200

    :sparkles: Added date service

--- evaluation/src/utility/__init__.py
+++ evaluation/src/utility/__init__.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from .date_service import DateService
 from .exception import KeyboardInterruptError
 from .file_service import FileService
 from .logging_service import LoggingService
@@ -7,5 +8,6 @@ from .logging_service import LoggingService
 __all__: List[str] = [
   "LoggingService",
   "FileService",
+  "DateService",
   "KeyboardInterruptError"
 ]
