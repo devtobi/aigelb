467213c24b9978b3a75bd6220893e5d7c31e4f30
devtobi <28538704+devtobi@users.noreply.github.com>
2025-06-21 22:03:20 +0200

    :recycle: Refactored 02_run_inference script

--- evaluation/src/02_run_inference.py
+++ evaluation/src/02_run_inference.py
@@ -1,38 +1,45 @@
-from os import getenv
-from typing import cast
+from typing import List
 
-from llama_cpp import (
-    ChatCompletionRequestSystemMessage,
-    ChatCompletionRequestUserMessage,
-    CreateChatCompletionResponse,
-    Llama,
-)
+from generation import GenerationService
+from model import Model, ModelService
+from utility import ConfigurationService, KeyboardInterruptError, LoggingService
 
-from utility import ConfigurationService, LoggingService
 
-# Check CPU / GPU mode
-ConfigurationService.load_environment_configuration()
-use_cpu: bool = getenv("USE_CPU") == "True"
-gpu_layers: int = 0 if use_cpu else -1
+def run_inference():
+  try:
+    models: List[Model] = ModelService.read_model_list()
+  except Exception as exc:
+    LoggingService.error(str(exc))
+    return
+  if not LoggingService.confirm_action("Are you sure you want to use those models for running inference?"):
+    return
+  try:
+    sources: List[str] = GenerationService.read_source_file()
+  except Exception as exc:
+    LoggingService.error(str(exc))
+    return
+  if not LoggingService.confirm_action("Are you sure you want to use those sentences for running inference?"):
+    return
+  try:
+    system_prompt: str = GenerationService.read_system_prompt()
+  except Exception as exc:
+    LoggingService.error(str(exc))
+    return
+  if not LoggingService.confirm_action("Are you sure you want to use this system prompt for running inference?"):
+    return
+  try:
+    user_prompt: str = GenerationService.read_user_prompt()
+  except Exception as exc:
+    LoggingService.error(str(exc))
+    return
+  if not LoggingService.confirm_action("Are you sure you want to use this user prompt for running inference?"):
+    return
+  try:
+    GenerationService.run_inference(models, sources, system_prompt, user_prompt)
+  except KeyboardInterruptError as exc:
+    LoggingService.error(str(exc))
+    return
 
-LoggingService.mute_llamacpp_logging()
-
-# load model
-llm: Llama = Llama.from_pretrained(
-    "TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
-    "tinyllama-1.1b-chat-v1.0.Q2_K.gguf",
-    local_files_only=True,
-    verbose=False,
-    n_gpu_layers=gpu_layers,
-)
-
-# execute inference
-system_message: ChatCompletionRequestSystemMessage = { "role": "system", "content": "You are an AI that always uses capital words when responding." }
-user_message: ChatCompletionRequestUserMessage = { "role": "user", "content": "Tell me a little story about bees." }
-completion_response: CreateChatCompletionResponse = cast(CreateChatCompletionResponse, llm.create_chat_completion(
-    messages=[system_message, user_message],
-    max_tokens=1000,
-))
-
-content: CreateChatCompletionResponse = completion_response
-print(content)
+if __name__ == "__main__":
+    ConfigurationService.load_environment_configuration()
+    run_inference()
