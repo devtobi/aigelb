05fbd13fa6784a78b40651ab53704386f81e9bf7
devtobi <28538704+devtobi@users.noreply.github.com>
2025-08-27 22:14:55 +0200

    :globe_with_meridians: Added localization

--- browser-extension/src/components/instructions/ImportantNotesCard.vue
+++ browser-extension/src/components/instructions/ImportantNotesCard.vue
@@ -1,21 +1,17 @@
 <template>
   <v-card
-    title="Wichtige Hinweise"
+    :title="i18n.t('instructions.importantNotesCard.title')"
     :prepend-icon="mdiAlert"
     color="error"
     class="pa-3"
     elevation="3"
   >
     <v-card-text>
-      <p>
-        AIGELB is currently in an
-        <strong>early development stage</strong>. Some translations may be
-        experimental or incomplete.
+      <p class="mb-3">
+        {{ i18n.t("instructions.importantNotesCard.description") }}
       </p>
       <p>
-        The project originates from accessibility research in
-        <strong>German Easy Language</strong> and aims to help make the web more
-        inclusive.
+        {{ i18n.t("instructions.importantNotesCard.development") }}
       </p>
     </v-card-text>
   </v-card>
@@ -23,5 +19,6 @@
 
 <script setup lang="ts">
 import { mdiAlert } from "@mdi/js";
+import { i18n } from "#i18n";
 import { VCard, VCardText } from "vuetify/components";
 </script>
