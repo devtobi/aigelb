630c55b5b13f87d856e334d5ab2d7b743189ed11
devtobi <28538704+devtobi@users.noreply.github.com>
2025-08-28 19:54:52 +0200

    :construction: Added InstructionsButton component

---
+++ browser-extension/src/components/popup/InstructionsButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-btn
+    :text="i18n.t('popup.instructionsButton.text')"
+    @click="openInstructions"
+    size="large"
+    block
+    color="warning"
+    :prepend-icon="mdiHelp"
+  />
+</template>
+
+<script setup lang="ts">
+import { mdiHelp } from "@mdi/js";
+import { i18n } from "#i18n";
+
+import { openInstructions } from "@/utility/browser.ts";
+</script>
