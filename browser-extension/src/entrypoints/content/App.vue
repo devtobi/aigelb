8baa4114ce703ba2b88e279b95ae037977944fd4
devtobi <28538704+devtobi@users.noreply.github.com>
2025-08-29 15:59:30 +0200

    :construction: Configured content script scaffolding

---
+++ browser-extension/src/entrypoints/content/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-btn>HELLO FROM AIGELB</v-btn>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
--- browser-extension/src/entrypoints/content/index.ts
+++ browser-extension/src/entrypoints/content/index.ts
@@ -1,8 +1,33 @@
 import { defineContentScript } from "#imports";
+import type { ContentScriptContext } from "wxt/utils/content-script-context";
+import { createApp } from "vue";
+import App from "@/entrypoints/content/App.vue";
+import { createShadowRootUi } from "wxt/utils/content-script-ui/shadow-root";
+import { registerVuePlugins } from "@/plugins";
 
 export default defineContentScript({
   matches: ["*://*/*"],
-  main() {
-    console.debug("Hello from AIGELB.");
-  },
+  cssInjectionMode: "ui",
+  async main(context: ContentScriptContext) {
+    const ui = await createShadowRootUi(context, {
+      name: 'aigelb-ui',
+      isolateEvents: true,
+      position: 'inline',
+      anchor: 'body',
+      onMount: (container) => {
+        const app = createApp(App);
+        registerVuePlugins(app);
+        app.mount(container);
+        return app;
+      },
+      onRemove: (app) => {
+        app?.unmount();
+      },
+    });
+
+    context.onInvalidated(() => {
+      console.debug("Content script got invalidated.");
+    });
+    ui.mount();
+  }
 });
