a2ddc76f460084447d20d4a39e709560a5117436
Tobias Stadler <28538704+devtobi@users.noreply.github.com>
2025-08-29 17:09:36 +0200

    🚧 Configured content script scaffolding (#252)

--- browser-extension/src/entrypoints/content/index.ts
+++ browser-extension/src/entrypoints/content/index.ts
@@ -1,8 +1,37 @@
+import type { ContentScriptContext } from "wxt/utils/content-script-context";
+
 import { defineContentScript } from "#imports";
+import { createApp } from "vue";
+import { createShadowRootUi } from "wxt/utils/content-script-ui/shadow-root";
+
+import App from "@/entrypoints/content/App.vue";
+import { registerVuePlugins } from "@/plugins";
 
 export default defineContentScript({
   matches: ["*://*/*"],
-  main() {
-    console.debug("Hello from AIGELB.");
+  cssInjectionMode: "ui",
+  async main(context: ContentScriptContext) {
+    const ui = await createShadowRootUi(context, {
+      name: "aigelb-ui",
+      isolateEvents: true,
+      position: "overlay",
+      anchor: "body",
+      onMount: (container) => {
+        const app = createApp(App);
+        registerVuePlugins(app);
+        app.mount(container);
+        return app;
+      },
+      onRemove: (app) => {
+        app?.unmount();
+      },
+    });
+
+    const notifyInvalidated = () => {
+      ui.remove();
+    };
+    context.onInvalidated(notifyInvalidated);
+
+    ui.mount();
   },
 });
