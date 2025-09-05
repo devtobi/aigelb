1a2c38d0decb5e53bf11f482163025b7a9ffb34b
devtobi <28538704+devtobi@users.noreply.github.com>
2025-09-05 19:04:19 +0200

    :wrench: Added additional inference configurations

--- browser-extension/src/config.ts
+++ browser-extension/src/config.ts
@@ -1,2 +1,19 @@
 export const LLM_HUGGINGFACE_REPO = "bartowski/Llama-3.2-1B-Instruct-GGUF";
 export const LLM_HUGGINGFACE_FILE = "Llama-3.2-1B-Instruct-Q4_K_M.gguf";
+export const LLM_SUPPORT_STREAMING = true;
+export const SYSTEM_PROMPT = `
+Sie sind ein Vereinfacher. Verwandeln Sie komplexe Sätze in einfaches Deutsch auf dem Leseniveau der 6.Klasse.
+- Verwenden Sie kurze Sätze und einfache Wörter.
+- Behalten Sie die Bedeutung bei.
+- Geben Sie nur den vereinfachten Text zurück.
+
+  Beispiele:
+Original: "Die Nutzung von Sonnenkollektoren kann die Energiekosten der Haushalte senken."
+Vereinfacht: "Mit Solarzellen können Sie Ihre Energierechnungen senken."
+Original: "Der Ausschuss traf eine einstimmige Entscheidung."
+Vereinfacht: "Alle Ausschussmitglieder waren sich einig."
+`;
+export const USER_PROMPT_TEMPLATE = `
+Bitte übersetzen Sie den folgenden Text ein einfaches Deutsch: {source}
+Bitte geben Sie nur den übersetzten Text zurück.
+`;
