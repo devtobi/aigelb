ebc6d67bc24ddab89aa558ae1f03a5521fd84507
devtobi <28538704+devtobi@users.noreply.github.com>
2025-09-06 11:33:27 +0200

    :lipstick: Added helper function to handle CSS syncing with Vuetify in Shadow Root

---
+++ browser-extension/src/utility/css.ts
@@ -0,0 +1,12 @@
+export async function syncCSSStyleSheets(stylesheetId: string, themeSheet: CSSStyleSheet, root: ShadowRoot) {
+    const headStyle = document.getElementById(stylesheetId) as HTMLStyleElement | null;
+    if (!headStyle) return;
+    const css = headStyle.textContent ?? "";
+    if ("replaceSync" in themeSheet) {
+      themeSheet.replaceSync(css);
+    }
+    const sheets = root.adoptedStyleSheets ?? [];
+    if (!sheets.includes(themeSheet)) {
+      root.adoptedStyleSheets = [...sheets, themeSheet];
+    }
+}
