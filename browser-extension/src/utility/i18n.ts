79f5ee9eee4d6b6a4203dc292254b0e4ded3761e
devtobi <28538704+devtobi@users.noreply.github.com>
2025-09-07 23:29:17 +0200

    :construction: Added function to get localized assets URLs

--- browser-extension/src/utility/i18n.ts
+++ browser-extension/src/utility/i18n.ts
@@ -1,5 +1,16 @@
+import { getUILanguage } from "@/utility/browser.ts";
+
 export function mapLangToVuetify(locale: string) {
   if (locale.startsWith("de")) return "de";
   if (locale.startsWith("en")) return "en";
   return "de"; // fallback
+}
+
+export function getAssetUrl(filename: string): string {
+  const lang = getUILanguage();
+  const localizedHref = new URL(`../assets/${lang}/${filename}`, import.meta.url).href;
+  if (localizedHref.indexOf("undefined") !== -1) {
+    return new URL(`../assets/en/${filename}`, import.meta.url).href;
+  }
+  return localizedHref;
 }
